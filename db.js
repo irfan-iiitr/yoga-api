9897730ed4f12c50809da2bb7de7327f2435feda
irfan-iiitr <imirfansari@gmail.com>
2024-01-10 19:44:09 +0530

    secured deployment

--- db.js
+++ db.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import { connect } from 'mongoose';
+import { config } from 'dotenv';
+// Configure dotenv
+config();
+
+
 // const mongoURI= 'mongodb+srv://aitools:irfan123@cluster0.1w1dr3f.mongodb.net/listdb?retryWrites=true&w=majority'
-const mongoURI="mongodb+srv://imirfansari:rW6OzumHEf1tyu5X@cluster0.c9rosml.mongodb.net/?retryWrites=true&w=majority"
+const mongoURI=process.env.URI
 //install mongodb mongose express nodemon
 //for nodeon in vite remove module in packagejson
 //remove <> in username password
